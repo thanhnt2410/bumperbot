45dbc339d190e0646bb4e9f335bd281cdb3d5c34
ThanhNT2 <thanh.ntt2402@gmail.com>
2025-11-17 01:29:41 +0700

    bumperbot_control

---
+++ src/bumperbot_py_example/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+package_name = 'bumperbot_py_example'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='thanh',
+    maintainer_email='thanh@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'simple_publisher = bumperbot_py_example.simple_publisher:main',
+            'simple_subscriber = bumperbot_py_example.simple_subscriber:main',
+            'simple_parameter = bumperbot_py_example.simple_parameter:main',
+            'simple_turtlesim_kinematics = bumperbot_py_example.simple_turtlesim_kinematics:main'
+        ],
+    },
+)
