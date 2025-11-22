660c4bc0668540d6f81530b22f8b4467495127b2
ThanhNT2 <thanh.ntt2402@gmail.com>
2025-11-23 01:23:40 +0700

    feat: Add GetTransform service and update related files for TF kinematics

--- src/bumperbot_cpp_example/include/bumperbot_cpp_example/simple_tf_kinematics.hpp
+++ src/bumperbot_cpp_example/include/bumperbot_cpp_example/simple_tf_kinematics.hpp
@@ -4,8 +4,12 @@
 #include <rclcpp/rclcpp.hpp>
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
 #include <memory>
 #include <geometry_msgs/msg/transform_stamped.hpp>
+#include <bumperbot_msgs/srv/get_transform.hpp>
+#include <tf2/LinearMath/Quaternion.h>
 
 class SimpleTfKinematics: public rclcpp::Node
 {
@@ -18,10 +22,21 @@ private:
     geometry_msgs::msg::TransformStamped static_transform_stamped_;
     geometry_msgs::msg::TransformStamped dynamic_transform_stamped_;
 
+    rclcpp::Service<bumperbot_msgs::srv::GetTransform>::SharedPtr get_transform_srv_;
+
     rclcpp::TimerBase::SharedPtr timer_;
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
     double x_increment_;
     double last_x_;
+    int rotations_counter_;
+    tf2::Quaternion last_orientation_;
+    tf2::Quaternion orientation_increment_;
+
     void timerCallback();
+
+    bool getTransformCallback(const std::shared_ptr<bumperbot_msgs::srv::GetTransform::Request> req,
+                            const std::shared_ptr<bumperbot_msgs::srv::GetTransform::Response> res);
 };
 
 #endif
