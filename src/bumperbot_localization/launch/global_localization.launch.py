ba43a8f4cdb5af7c86ab8d2fc3fc46ea7b9341b1
ThanhNT2 <thanh.ntt2402@gmail.com>
2026-05-29 02:55:36 +0700

    lifecycle manager

---
+++ src/bumperbot_localization/launch/global_localization.launch.py
@@ -0,0 +1,60 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="true"
+    )
+
+    map_name_arg = DeclareLaunchArgument(
+        "map_name",
+        default_value="small_house"
+    )
+
+    map_name = LaunchConfiguration("map_name")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+
+    lifecycle_nodes = ["map_server"]
+
+
+    map_path = PathJoinSubstitution([
+        get_package_share_directory("bumperbot_mapping"),
+        "maps",
+        map_name,
+        "map.yaml"
+    ])
+
+    nav2_map_server = Node(
+        package="nav2_map_server",
+        executable="map_server",
+        output="screen",
+        parameters=[
+            {"yaml_filename": map_path},
+            {"use_sim_time": use_sim_time}
+        ]
+    )
+
+    nav2_lifecycle_manager = Node(
+        package="nav2_lifecycle_manager",
+        executable="lifecycle_manager",
+        name="lifecycle_manager_localization",
+        output="screen",
+        parameters=[
+            {"use_sim_time": use_sim_time},
+            {"autostart": True},
+            {"node_names": lifecycle_nodes}
+        ]
+    )
+
+    return LaunchDescription([
+        map_name_arg,
+        use_sim_time_arg,
+        nav2_map_server,
+        nav2_lifecycle_manager
+    ])
